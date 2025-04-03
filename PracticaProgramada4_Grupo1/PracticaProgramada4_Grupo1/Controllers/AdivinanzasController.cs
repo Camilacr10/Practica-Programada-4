75a359100316cec48bec7e14c39744586662d6fa
Dramirez00467 <106043587+Dramirez00467@users.noreply.github.com>
2025-04-02 22:46:30 -0600

    Punto 3 y 4 (Diana y Camila)
    Puntos realizados por Diana Ramirez y Camila Corrales

---
+++ PracticaProgramada4_Grupo1/PracticaProgramada4_Grupo1/Controllers/AdivinanzasController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PracticaProgramada4_Grupo1.Models;
+
+namespace PracticaProgramada4_Grupo1.Controllers
+{
+    public class AdivinanzasController : ApiController
+    {
+            [HttpGet]
+            [Route("ObtenerAdivinanza")]
+            public IHttpActionResult ObtenerAdivinanza()
+            {
+                return Ok(new { Pregunta = "Blanca por dentro, verde por fuera. Si quieres que te lo diga, espera." });
+            }
+
+
+
+
+        [HttpPost]
+        [Route("ResponderAdivinanza")]
+        public IHttpActionResult ResponderAdivinanza([FromBody] string respuesta)
+        {
+            bool correcta = respuesta.ToLower() == "la pera";
+            return Ok(new
+            {
+                Correcta = correcta,
+                RespuestaCorrecta = "La pera"
+            });
+        }
+    }
+    }
---
+++ PracticaProgramada4_Grupo1/PracticaProgramada4_Grupo1/Models/Adivinanza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticaProgramada4_Grupo1.Models
+{
+	public class Adivinanza
+    {
+        public string Pregunta { get; set; }
+        public string Respuesta { get; set; }
+    }
+
+    public class RespuestaAdivinanza
+    {
+        public string RespuestaUsuario { get; set; }
+    }
+}
