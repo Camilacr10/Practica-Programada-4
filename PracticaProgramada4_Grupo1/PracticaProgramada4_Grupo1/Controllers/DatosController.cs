279dc580900418434ae69a8778dd768cbbe9b6b8
JasonChavarria07 <108899160+JasonChavarria07@users.noreply.github.com>
2025-04-07 17:33:21 -0600

    Datos curiosos

---
+++ PracticaProgramada4_Grupo1/PracticaProgramada4_Grupo1/Controllers/DatosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace PracticaProgramada4_Grupo1.Controllers
+{
+    // Controlador para manejar los datos curiosos
+    public class DatosController : ApiController
+    {
+        private static readonly List<string> Datos = new List<string>
+        {
+            "El primer avión voló el 17 de diciembre de 1903.",
+            "La probabilidad de que exista una langosta azul es de una entre dos millones.",
+            "Se puede escuchar el latido de una ballena azul a más de 3 kilómetros de distancia.",
+            "Una gota de petróleo puede contaminar 25 litros de agua.",
+            "Es imposible estornudar con los ojos abiertos."
+        };
+
+        private static readonly Random rng = new Random();
+
+        // Método GET para obtener un dato curioso
+        [HttpGet]
+        [Route("ObtenerDato")] // Ruta para obtener el dato curioso
+        public IHttpActionResult ObtenerDato()
+        {
+            int indice = rng.Next(Datos.Count);
+            return Ok(new { Mensaje = Datos[indice] });
+        }
+    }
+}
